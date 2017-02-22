a1206802c3c840ed5523f8255aeca384faafd103
Brian Stien <brian.stien@moneydesktop.com>
2017-02-22 10:56:39 -0700

    Extract bulk methods from active_remote

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,16 @@
-require "bundler/setup"
-require "active_remote/bulk"
+# frozen_string_literal: true
+require 'bundler/setup'
+require 'active_remote/bulk'
+
+require 'pry'
+
+$LOAD_PATH << ::File.expand_path('../support/protobuf', __FILE__)
+require 'support/protobuf'
+require 'support/models'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
